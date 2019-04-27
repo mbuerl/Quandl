85ddc7616b4ad9b244bff491b187623eabb553aa
mbuerl <fantasticbashflash@gmail.com>
2019-04-27 21:11:01 +0200

    matthias try

--- quandlpull.py
+++ quandlpull.py
@@ -2,5 +2,5 @@ import quandl
 
 
 quandl.ApiConfig.api_key = 'jhkghJ59ybRRaM1E7QeK'
-stock = quandl.get('FSE/EON_X', column_index='1')
+stock = quandl.get('FSE/WDI_X', column_index='1')
 print(stock)
